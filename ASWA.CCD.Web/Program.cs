9a710015beef91b28134caa4b84966c02a6ac513
Rick van den Bosch <rickvdbosch@outlook.com>
2026-06-09 17:25:50 +0200

    Package update Removed Azure.Identity package

--- ASWA.CCD.Web/Program.cs
+++ ASWA.CCD.Web/Program.cs
@@ -2,7 +2,6 @@
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Azure.Identity;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
