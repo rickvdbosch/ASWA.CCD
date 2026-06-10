c270f5f8d322f0a25bb1af34b5e4661bff752615
Rick van den Bosch <rickvdbosch@outlook.com>
2026-06-10 09:24:57 +0200

    Add IsPast and IsActive to the session model

--- ASWA.CCD.Common/Models/SessionModel.cs
+++ ASWA.CCD.Common/Models/SessionModel.cs
@@ -1,14 +1,15 @@
-﻿namespace ASWA.CCD.Common.Models
+﻿namespace ASWA.CCD.Common.Models;
+
+public class SessionModel
 {
-    public class SessionModel
-    {
-        public string Id { get; set; }
-        public string Start { get; set; }
-        public string End { get; set; }
-        public int Duration { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Speaker { get; set; }
-        public string Room { get; set; }
-    }
+    public string Id { get; set; }
+    public string Start { get; set; }
+    public string End { get; set; }
+    public int Duration { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Speaker { get; set; }
+    public string Room { get; set; }
+    public bool IsPast { get; set; }
+    public bool IsActive { get; set; }
 }
